2855c1acf9caff11198e9199af861cfc354b49ba
ruhsam <ruhsam@gmail.com>
2016-05-27 13:59:20 -0700

    corrected phone number data type

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527205533) do
+ActiveRecord::Schema.define(version: 20160527205805) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.integer  "phone_number"
+    t.string   "phone_number"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
